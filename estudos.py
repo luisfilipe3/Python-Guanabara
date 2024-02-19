c284b214672472029c2faa4b1ee88e3c01ba966b
luisfilipe3 <135170965+luisfilipe3@users.noreply.github.com>
2024-02-19 00:16:17 -0300

    Add files via upload

---
+++ estudos.py
@@ -0,0 +1,34 @@
+# Expressões
+# Operadores aritméticos ( +, -, *, /, //, %, **)
+'''Precedência de operadores
+    1° = ( )
+    2° = **
+    3° = *  /  // 
+    4° = %
+    5° = +  -                 '''
+# Expressões lógicas (==, !=, <, >, <=, >=, and, or, not)
+#
+# Strings(aspas simples e duplas)
+#
+# Variáveis
+#
+# Saída (f-strings)
+# Entrada (string, int, float)
+# // eh o operador que vai arredondar o resultado e ignorar os numeros apos a virgula ou ponto
+# % esse operador retorna o resto que sobra de diferenca
+# ** eh o operador"
+# Comparador para igual
+# Comparador para diferença
+# And comparador que as 2 afirmações sejam falsas ou verdadeiras para vir o resultado esperado,
+#
+#
+# Aspas simples
+# Para limitar 2 números após a virgula: print(f'O seu salário atual é R${salario:.2f}
+# usando um print(f'xxxxxx') você vai usar {objeto:.2f} o número antes do f define até quantas
+# casas você vai poder mostrar após a virgula(ponto final aqui)
+#
+#
+#
+print ('"ola" tudo bem')
+numero = 5
+print (numero)

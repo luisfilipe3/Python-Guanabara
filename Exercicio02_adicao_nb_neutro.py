62efca3a1243d6a14592ac9e1f0a0a12261ce234
luisfilipe3 <135170965+luisfilipe3@users.noreply.github.com>
2024-02-24 10:43:09 -0300

    Add files via upload

---
+++ Exercicio02_adicao_nb_neutro.py
@@ -0,0 +1,12 @@
+nome = input ('Digite o seu nome por favor :D \n')
+genero = input('Digite a letra que você mais se identifica:\nH para Homem(Cis)\nM para Mulher(Cis)\nL para LGBTQIAP+\nNB para pessoa Não-Binária\n')
+if genero == 'H' or genero == 'h':
+    print(f'Seja Bem Vindo {nome}')
+elif genero == 'M' or genero == 'm':
+    print(f'Seja Bem Vinda {nome}')
+elif genero == 'L' or genero == 'l':
+    print(f'Seja Bem Vinde {nome}')
+elif genero == 'NB' or genero == 'nb':
+    print(f'Seja Bem Vindu {nome}')
+else:
+    print('Insira uma das opções sugeridas por favor.')
